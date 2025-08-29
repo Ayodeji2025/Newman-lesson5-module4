96d18acfab7c9633bd4ee514ae591226bd254ec2
Ayodeji2025 <adeyemiadetona17@gmail.com>
2025-08-29 01:55:05 +0100

    updata Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+package MYjavaworkingtesttask;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world this is Ayodeji Adeyemi,This is my first java work on intellij and am planning to be a great software tester worldwide");
+        System.err.println("dont be to nice to everyone");
+        System.out.printf("I love Jesus and God grace all i can see");
+
+    }
+}
